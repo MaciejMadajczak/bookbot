3ee4f03bc93d6676ce729380dba0a63eeb8a0c23
Maciej Mat-Madajczak <maciej.madajczak@gmail.com>
2025-04-08 12:29:19 +0200

    main.py now contains get_book_text function to read the file.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+def main():
+    path_to_file = "/books/frankenstein.txt"
+    print(get_book_text(path_to_file))
+
+main()
